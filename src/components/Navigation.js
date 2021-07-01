c5a162c09e8bb7d5b421438fa21b2f535de54c26
Nasir Uddin <md.nasir8534@gmail.com>
2021-07-01 21:54:06 +0600

    Sticky Menu and Project Gallery Completed

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,32 +1,34 @@
-import React, { Component } from 'react';
+import React,{useState} from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import logo from '../images/logo.png';
 import './css/Navigation.css';
-class Navigation extends Component {
-
-    changebackground = () => {
-        console.log(window.scrollY);
+function Navigation() {
+    const[nav_area,setbackground]=useState(false);
+    let changeBackground=()=>{
+        if(window.scrollY>=80){
+            setbackground(true);
+            console.log(window.scrollY);
+        }  
+        else
+        setbackground(false);
     }
-    render() {
-        return (
-            <div className="nav-area sticky-top">
-                <div>
-                    <Navbar className="container" expand="lg">
-                        <Navbar.Brand href="#home"><img src={logo} alt="logo" /></Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="ml-auto">
-                                <Nav.Link className="active" href="#home">Home</Nav.Link>
-                                <Nav.Link href="#service">Services</Nav.Link>
-                                <Nav.Link href="#about">About</Nav.Link>
-                                <Nav.Link href="#contact">Contact</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Navbar>
-                </div>
-            </div >
-        )
-    }
+    window.addEventListener('scroll',changeBackground)
+    return (
+        <div className={nav_area? 'nav_area activated':'nav_area'}>
+            <Navbar className="container" bg="tranparent" expand="lg">
+                <Navbar.Brand href="#home"><img src={logo} alt="logo" /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link className="active" href="#home">Home</Nav.Link>
+                            <Nav.Link href="#service">Services</Nav.Link>
+                            <Nav.Link href="#about">About</Nav.Link>
+                            <Nav.Link href="#contact">Contact</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+        </div >
+    )
 }
 
 export default Navigation
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -14,34 +14,30 @@ export const Portfolio = () => {
         <div className="portfolio-area">
             <div className="container-fluid">
                 <h2>Recently Done Project<br /> Quality Work</h2>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6 col-md-offset-6">
-                            <div className="button-group">
-                                <button className="btn" onClick={() => setitem(Menu)}>all</button>
-                                <button className="btn" onClick={() => filterItem('popular')}>popular</button>
-                                <button className="btn" onClick={() => filterItem('latest')}>latest</button>
-                                <button className="btn" onClick={() => filterItem('following')}>following</button>
-                                <button className="btn" onClick={() => filterItem('upcoming')}>upcoming</button>
-                            </div>
+                <div className="row">
+                    <div className="col-md-7">
+                        <div className="button-group">
+                            <button className="btn first" onClick={() => setitem(Menu)}>all</button>
+                            <button className="btn" onClick={() => filterItem('popular')}>popular</button>
+                            <button className="btn" onClick={() => filterItem('latest')}>latest</button>
+                            <button className="btn" onClick={() => filterItem('following')}>following</button>
+                            <button className="btn" onClick={() => filterItem('upcoming')}>upcoming</button>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="menu-content">
-                            {
-                                item.map((e) => {
-                                    return (
-                                        <div className="col-md-12">
-                                            <div className="content img-fluid">
-                                                <img src={e.image} alt="image1" />
-                                                <h4>{e.name}</h4>
-                                                <p>{e.description}</p>
-                                            </div>
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
+                </div>
+                <div className="row portfolio-content">
+                    <div className="menu-content">
+                        {
+                            item.map((e) => {
+                                return (
+                                    <div className="content" key={e.id}>
+                                        <img className="content-img" src={e.image} alt="image1" />
+                                        <h4>{e.name}</h4>
+                                        <p>{e.description}</p>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                 </div>
             </div>
